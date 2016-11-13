c135bc09127263d36072629b3226a0d22059f67e
bugrovtolik <bugrovtolik@gmail.com>
2016-11-13 23:55:51 +0200

    created MapBorder.js, MapClass.js, MapObject.js

--- classes/Map.js
+++ classes/Map.js
@@ -1,3 +1,21 @@
-class Map {
-	
+import PIXI from 'PIXI'
+import MapClass from './MapClass'
+import MapBorder from './MapBorder'
+
+class Map extends MapClass {
+	constructor(width, height) {
+		super(width, height);
+		this.sprite = new PIXI.extras.TilingSprite(new PIXI.Texture.fromImage('images/background.jpg'), width, height);
+		this.borders = [new MapBorder(width,20,'images/border.png'), 
+						new MapBorder(20,height,'images/border.png'), 
+						new MapBorder(width,20,'images/border.png'), 
+						new MapBorder(20,height,'images/border.png')];
+		this.borders[1].getSprite().position.x = width-20;
+		this.borders[2].getSprite().position.y = height-20;
+	}
+	getSprite() {
+		return this.sprite;
+	}
 }
+
+export default Map
--- main.js
+++ main.js
@@ -1,65 +1,18 @@
 import PIXI from "PIXI"
+import Map from './classes/Map'
 
-var renderer = PIXI.autoDetectRenderer(1000, 600,{backgroundColor : 0xffffff});
+var renderer = PIXI.autoDetectRenderer(1024, 600,{backgroundColor : 0xffffff});
 document.body.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-var postition = 0,
-	background,
-    background2,
-    foreground,
-    foreground2;
+const map = new Map(2000,2000);
 
-background = PIXI.Sprite.fromImage('./images/background.jpg');
-background2 = PIXI.Sprite.fromImage('./images/background2.jpg');
-stage.addChild(background);
-stage.addChild(background2);
-
-foreground = PIXI.Sprite.fromImage('./images/background.jpg');
-foreground2 = PIXI.Sprite.fromImage('./images/background2.jpg');
-stage.addChild(foreground);
-stage.addChild(foreground2);
-foreground.position.y = foreground2.position.y = 640 - foreground2.height;
-
+stage.addChild(map.getSprite());
+for(var i = 0; i < 4; i++) stage.addChild(map.borders[i].getSprite());
 
 animate();
 function animate() {
-    postition += 10;
-
-    background.position.x = -(postition * 0.6);
-    background.position.x %= 1286 * 2;
-    if(background.position.x < 0)
-    {
-        background.position.x += 1286 * 2;
-    }
-    background.position.x -= 1286;
-
-    background2.position.x = -(postition * 0.6) + 1286;
-    background2.position.x %= 1286 * 2;
-    if(background2.position.x < 0)
-    {
-        background2.position.x += 1286 * 2;
-    }
-    background2.position.x -= 1286;
-
-    foreground.position.x = -postition;
-    foreground.position.x %= 1286 * 2;
-    if(foreground.position.x < 0)
-    {
-        foreground.position.x += 1286 * 2;
-    }
-    foreground.position.x -= 1286;
-
-    foreground2.position.x = -postition + 1286;
-    foreground2.position.x %= 1286 * 2;
-    if(foreground2.position.x < 0)
-    {
-        foreground2.position.x += 1286 * 2;
-    }
-    foreground2.position.x -= 1286;
-
-    requestAnimationFrame(animate);
-
-    renderer.render(stage);
+	requestAnimationFrame(animate);
+	renderer.render(stage);
 }
